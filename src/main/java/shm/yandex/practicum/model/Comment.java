887d304886294fef06f4728d39e3594aa870b8ed
Oleg Shmelev <o.shmelev76@gmail.com>
2026-02-06 01:24:16 +0300

    +modify models using Lombok

--- src/main/java/shm/yandex/practicum/model/Comment.java
+++ src/main/java/shm/yandex/practicum/model/Comment.java
@@ -1,41 +1,16 @@
 package shm.yandex.practicum.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
     private Long id;
     private String text;
     private Long postId;
-
-
-    public Comment() {
-    }
-
-    public Comment(Long id, String text, Long postId) {
-        this.id = id;
-        this.text = text;
-        this.postId = postId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public Long getPostId() {
-        return postId;
-    }
-
-    public void setPostId(Long postId) {
-        this.postId = postId;
-    }
 }
